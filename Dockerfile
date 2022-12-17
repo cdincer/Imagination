95e0d35c54041528cbd57732855e205ab587059c
cdincer <dincercan@gmail.com>
2022-12-17 21:02:20 +0300

    TakeAsync functional creating items

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,21 @@
 # Feel free to replace this with a more appropriate Dockerfile.
 
+FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+WORKDIR /src
+COPY ["src/Imagination.Server/Imagination.Server.csproj", "src/Imagination.Server/"]
+RUN dotnet restore "src/Imagination.Server/Imagination.Server.csproj"
+COPY . .
+WORKDIR "/src/src/Imagination.Server"
+RUN dotnet build "Imagination.Server.csproj" -c Release -o /app/build
+
+FROM build AS publish
+RUN dotnet publish "Imagination.Server.csproj" -c Release -o /app/publish /p:UseAppHost=false
+
+FROM base AS final
+WORKDIR /app
+COPY --from=publish /app/publish .
+ENTRYPOINT ["dotnet", "Imagination.Server.dll"]
+
 FROM alpine
 RUN apk update
 CMD ["apk", "fetch", "coffee"]
--- src/Imagination.Server/Controllers/ConvertController.cs
+++ src/Imagination.Server/Controllers/ConvertController.cs
@@ -1,8 +1,17 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Drawing;
+using System.Collections.Specialized;
+using System.Text;
 
 namespace Imagination.Controllers
 {
@@ -23,9 +32,30 @@ namespace Imagination.Controllers
         }
 
         [HttpPost]
-        public void Get()
+        public async Task TakeAsync( CancellationToken cancellationToken)
         {
-            Console.WriteLine("aaaa");
+            var myRequest = Request;
+            Stream requestBody = myRequest.Body;
+            //Goal is stream to byte array
+            //string path = @"C:\Users\Can\Documents\Visual Studio Code\Imagination\resources2\"+Guid.NewGuid()+".jpg";
+            byte[] items;
+            using (var memoryStream = new MemoryStream())
+            {
+                await requestBody.CopyToAsync(memoryStream);
+                items = memoryStream.ToArray();
+            }
+
+
+            string path = @"C:\Users\Can\Documents\Visual Studio Code\Imagination\resources2\"+Guid.NewGuid()+".jpg";
+            using (FileStream fs = System.IO.File.Create(path))
+            {
+                // Add some information to the file.
+                fs.Write(items, 0, items.Length);
+            }
+
+            Console.WriteLine("aaa");
+
+            Console.WriteLine("bbb");
         }
     }
 }
